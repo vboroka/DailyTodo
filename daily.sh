22bf7b2e2cd938a39da6e47b36b0c798a79bc95e
v.boro <73988372+vboroka@users.noreply.github.com>
2023-03-29 21:56:35 +0200

    "remove_task" függvény

--- daily.sh
+++ daily.sh
@@ -11,3 +11,12 @@ function add_task {
     echo "Az új tétel hozzáadva az alábbiakhoz:"
     echo "${napirend[@]}"
 }
+
+# A "remove_task" függvénnyel feladatokat lehet eltávolítani a napirendből
+function remove_task {
+    echo "Melyik tételt szeretnéd törölni? Adja meg a sorszámát:"
+    read index
+    unset napirend[$index-1]
+    echo "A kiválasztott tétel törölve lett. Az új napirend:"
+    echo "${napirend[@]}"
+}
