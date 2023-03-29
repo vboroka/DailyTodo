9b28cec68e8521e1606221e6a882def2c4b3e604
v.boro <73988372+vboroka@users.noreply.github.com>
2023-03-29 21:00:58 +0200

    "add_task" függvény

--- daily.sh
+++ daily.sh
@@ -2,3 +2,12 @@
 
 # Az "napirend.txt" fájl beolvasása a "napirend" tömbbe
 IFS=$'\n' read -d '' -r -a napirend < napirend.txt
+
+# Az "add_task" függvény, amely lehetővé teszi az új napirendi tétel hozzáadását a "napirend" tömbhöz
+function add_task {
+    echo "Add meg az új tételt:"
+    read new_task
+    napirend+=("$new_task")
+    echo "Az új tétel hozzáadva az alábbiakhoz:"
+    echo "${napirend[@]}"
+}
