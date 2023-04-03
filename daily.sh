834e6b4ef60ac870279971724c19fd540e764656
v.boro <73988372+vboroka@users.noreply.github.com>
2023-04-03 19:43:56 +0200

    "show_list" és "show_menu" függvények hozzáadása

--- daily.sh
+++ daily.sh
@@ -20,3 +20,22 @@ function remove_task {
     echo "A kiválasztott tétel törölve lett. Az új napirend:"
     echo "${napirend[@]}"
 }
+
+# A "show_list" függvény kiíratja a napirendet.
+function show_list {
+    echo "A napirend:"
+    echo "${napirend[@]}"
+}
+
+# A "show_menu" függvény jeleníti meg nekünk azokat az opciókat, amelyeke
+function show_menu {
+    echo "Napirend alkalmazás"
+    echo "-------------------"
+    echo "1. Napirend megjelenítése"
+    echo "2. Napirendi tétel hozzáadása"
+    echo "3. Napirendi tétel törlése"
+    echo "4. Kilépés"
+    echo "-------------------"
+    echo
+    echo -n "Válassz egy menüpontot [1-4]: "
+}
