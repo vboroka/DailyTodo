a307d4da1b7f8173a15f06eb7f52d606860d13c4
v.boro <73988372+vboroka@users.noreply.github.com>
2023-04-04 11:45:06 +0200

    Végső simítás 2.

--- daily.sh
+++ daily.sh
@@ -5,48 +5,53 @@ file="napirend.txt"
 
 # Ellenőrizzük, hogy létezik-e a fájl, ha nem, létrehozzuk
 if [ ! -f $file ]; then
-    touch $file
+     touch $file
 fi
 
 # Kilistázzuk a napirendet
 function list {
-    echo "A napirended:"
-    echo "--------------"
-    cat $file
+     echo ""
+     echo "[ A napirended ]"
+     cat $file
+     echo "--------------"
 }
 
 # Hozzáadjuk az új feladatot a napirendhez
 function add {
-    echo "Add meg az új feladatot:"
-    read task
-    echo $task >> $file
-    echo "Feladat hozzáadva a napirendhez."
+     echo ""
+     echo "Add meg az új feladatot:"
+     read task
+     echo $task >> $file
+     echo ">> Feladat hozzáadva a napirendhez."
 }
 
 # Töröljük a megadott feladatot a napirendből
 function remove {
-    echo "Add meg a törölni kívánt feladat sorszámát:"
-    read num
-    sed -i "${num}d" $file
-    echo "Feladat törölve a napirendből."
+     echo ""
+     echo "Add meg a törölni kívánt feladat sorszámát:"
+     read num
+     sed -i "${num}d" $file
+     echo ">> Feladat törölve a napirendből."
 }
 
 # Az alkalmazás főciklusa
 while true
 do
-    echo ""
-    echo "Mit szeretnél tenni?"
-    echo "1. Napirend kilistázása"
-    echo "2. Új feladat hozzáadása"
-    echo "3. Feladat törlése"
-    echo "4. Kilépés"
-    read choice
+     echo ""
+     echo "Mit szeretnél tenni?"
+     echo ">> 1. Napirend kilistázása"
+     echo ">> 2. Új feladat hozzáadása"
+     echo ">> 3. Feladat törlése"
+     echo ">> 4. Kilépés"
+     echo ""
+     echo "Add meg választásod: "
+read choice
 
-    case $choice in
-        1) list;;
-        2) add;;
-        3) remove;;
-        4) exit;;
-        *) echo "Nem értem a parancsot.";;
-    esac
+     case $choice in
+         1) list;;
+         2) add;;
+         3) remove;;
+         4) exit;;
+         *) echo ">> Nem értem a parancsot.";;
+     esac
 done
